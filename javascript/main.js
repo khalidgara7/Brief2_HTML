0f031ce449f667bb2c486864f43f711ce37c6f9e
khalid <khalidgara8@gmail.com>
2023-10-25 23:49:46 +0200

    modifier local storage

--- javascript/main.js
+++ javascript/main.js
@@ -25,3 +25,4 @@ function handlePanier(){
         panierContent.classList.add("hidden")
     }
 }
+
--- javascript/script.js
+++ javascript/script.js
@@ -1,36 +1,38 @@
 // -------------- Locale storeg and validation ---------------
 
-let First_Name  = document.querySelector("#first_name");
-let Last_Name  = document.querySelector("#last_name");
-let Phone_Number  = document.querySelector("#phone_number");
+let First_Name = document.querySelector("#first_name");
+let Last_Name = document.querySelector("#last_name");
+let Phone_Number = document.querySelector("#phone_number");
 let Email = document.querySelector("#email");
-let message_contact  = document.querySelector("#message-contact");
+let message_contact = document.querySelector("#message-contact");
+// -è----------------
+let dec = document.getElementById("decrement");
+let inc = document.getElementById("increment");
+const Qnt = document.getElementById("Quantite");
+let prix = document.getElementById("prix");
 
-
-let users = JSON.parse(localStorage.getItem('object')) || [];
-let F,L,Ph,E,M;
-let obj =[]
-function check(){
-let obj1 = [
-        F = First_Name.value,
-        L = Last_Name.value ,
-        Ph = Phone_Number.value ,
-        E = Email.value,
-        M = message_contact.value
-    ]
-    obj.push(obj1)
-    localStorage.setItem('object',JSON.stringify(obj));
-    
+let users = JSON.parse(localStorage.getItem("object")) || {};
+let F, L, Ph, E, M;
+let obj = [];
+function check() {
+  let obj1 = {
+    F: First_Name.value,
+    L: Last_Name.value,
+    Ph: Phone_Number.value,
+    E: Email.value,
+    M: message_contact.value,
+  };
+  obj.push(obj1);
+  localStorage.setItem("object", JSON.stringify(obj));
 }
 
-var test  = "";
+var test = "";
 let storedData = localStorage.getItem("object");
 var tableContent = "";
-function showUser(){
-    
-for (let index = 0; index < users.length; index++) {
-    users[index][0].split(',');
-    let arrr =users[index]
+function showUser() {
+  for (let index = 0; index < users.length; index++) {
+    users[index][0].split(",");
+    let arrr = users[index];
     tableContent += `
     <tr>
         <th>${arrr[0]}</th>
@@ -39,8 +41,7 @@ for (let index = 0; index < users.length; index++) {
         <th>${arrr[3]}</th>
         <th>${arrr[4]}</th>
     </tr> `;
-}
-       
+  }
 }
 
 function validateEmail(email) {
@@ -50,19 +51,15 @@ function validateEmail(email) {
 
 //--------------- incre/decre---------------------
 
-let dec = document.getElementById("decrement");
-let inc = document.getElementById("increment");
-let Qnt = document.getElementById("Quantite");
-let prix = document.getElementById("prix");
-
-function decrementEvent(){
-   Qnt.innerText = parseInt(Qnt.innerText)-1;
-   console.log(Qnt.InnerText ,Qnt.innerText);
-   prix.innerText = parseInt(prix.innerText) * parseInt(Qnt.innerText)  ;
+function decrementEvent() {
+  if (Qnt.innerText > 1) {
+    Qnt.innerText = parseInt(Qnt.innerText) - 1;
+    prix.innerText = parseInt(prix.innerText) - 1900;
+    console.log(Qnt.innerText, prix.innerText);
+  }
 }
-function incrementEvent(){
-    Qnt.innerText = parseInt(Qnt.innerText)+1; 
-    console.log(Qnt.InnerText ,Qnt.innerText);
-    prix.innerText = parseInt(prix.innerText) * parseInt(Qnt.innerText) ;
- }
- console.log("hhhh")
+function incrementEvent() {
+  Qnt.innerText = parseInt(Qnt.innerText) + 1;
+  prix.innerText = parseInt(prix.innerText) + 1900;
+  console.log(Qnt.innerText, prix.innerText);
+}
